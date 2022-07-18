697599d1d8f69b339d67a7d64a0efef4fa856c09
raviteja83 <ravi.theja@100ms.live>
2022-07-18 18:45:11 +0530

    feat: initial working pan

--- apps/100ms-web/src/components/hooks/useVideoZoom.js
+++ apps/100ms-web/src/components/hooks/useVideoZoom.js
@@ -10,11 +10,14 @@ export const useVideoZoom = () => {
     const max_scale = 1.425;
     let scale = 1;
     const element = ref.current;
+    let start = { x: 0, y: 0 };
+    let last = { x: 0, y: 0 };
+    let isDown = false;
     document.addEventListener("keydown", e => {
       if (e.key === "Escape" && element.style.transform) {
         e.stopPropagation();
-        element.style.transform = "scale(1)";
-        element.style["transform-origin"] = "0 0";
+        scale = 1;
+        element.style.transform = `matrix(${scale}, 0 , 0, ${scale}, 0, 0)`;
       }
     });
     element.addEventListener("wheel", e => {
@@ -22,10 +25,39 @@ export const useVideoZoom = () => {
       scale += e.deltaY * -factor;
       // Restrict scale
       scale = Math.min(Math.max(1, scale), max_scale);
-      element.style["transform-origin"] = `${e.clientX}px ${e.clientY}px`;
       // Apply scale transform
-      element.style.transform = `scale(${scale})`;
+      element.style.transform = `matrix(${scale}, 0 , 0, ${scale}, 0, 0)`;
     });
+
+    function startDrag(event) {
+      start.x = event.clientX - last.x;
+      start.y = event.clientY - last.y;
+      isDown = true;
+    }
+
+    function stopDrag(event) {
+      isDown = false;
+      last.x = event.clientX - start.x;
+      last.y = event.clientY - start.y;
+    }
+
+    function whileDrag(event) {
+      const currXPos = event.clientX;
+      const currYPos = event.clientY;
+      // Allow pan only when scaled
+      if (scale === 1 || !isDown) {
+        return;
+      }
+      const position = {
+        x: currXPos - start.x,
+        y: currYPos - start.y,
+      };
+      element.style.transform = `matrix(${scale}, 0 , 0, ${scale}, ${position.x}, ${position.y})`;
+    }
+
+    element.addEventListener("mousedown", startDrag);
+    element.parentElement.addEventListener("mousemove", whileDrag);
+    element.parentElement.addEventListener("mouseup", stopDrag);
   }, []);
   return ref;
 };
