22d55f521caa014f7946b86b67ffcc347956ec3c
raviteja83 <ravi.theja@100ms.live>
2022-11-29 15:21:58 +0530

    fix: try a test plugin to see frame freeze

---
+++ apps/100ms-web/src/plugins/VirtualBackground/TestPlugin.js
@@ -0,0 +1,27 @@
+export class TestPlugin {
+  init() {}
+  checkSupport() {
+    return {
+      isSupported: true,
+    };
+  }
+  processVideoFrame(input, output) {
+    output.width = input.width;
+    output.height = input.height;
+    const ctx = output.getContext("2d");
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+    ctx.fillRect(0, 0, input.width, input.height);
+    ctx.globalCompositeOperation = "destination-over";
+    ctx.drawImage(input, 0, 0, input.width, input.height);
+  }
+
+  getName() {
+    return "TestPlugin";
+  }
+
+  getPluginType() {
+    return "TRANSFORM";
+  }
+
+  stop() {}
+}
--- apps/100ms-web/src/plugins/VirtualBackground/VirtualBackground.jsx
+++ apps/100ms-web/src/plugins/VirtualBackground/VirtualBackground.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { HMSVirtualBackgroundTypes } from "@100mslive/hms-virtual-background";
+// import { HMSVirtualBackgroundTypes } from "@100mslive/hms-virtual-background";
 import {
   selectIsAllowedToPublish,
   selectIsLocalVideoPluginPresent,
@@ -11,7 +11,8 @@ import {
 import { VirtualBackgroundIcon } from "@100mslive/react-icons";
 import { Tooltip } from "@100mslive/react-ui";
 import IconButton from "../../IconButton";
-import { getRandomVirtualBackground } from "./vbutils";
+import { TestPlugin } from "./TestPlugin";
+// import { getRandomVirtualBackground } from "./vbutils";
 
 export const VirtualBackground = () => {
   const pluginRef = useRef(null);
@@ -20,15 +21,18 @@ export const VirtualBackground = () => {
   const role = useHMSStore(selectLocalPeerRole);
   const [isVBSupported, setIsVBSupported] = useState(false);
   const localPeerVideoTrackID = useHMSStore(selectLocalVideoTrackID);
-  const isVBPresent = useHMSStore(selectIsLocalVideoPluginPresent("HMSVB"));
+  const isVBPresent = useHMSStore(
+    selectIsLocalVideoPluginPresent("TestPlugin")
+  );
 
   async function createPlugin() {
     if (!pluginRef.current) {
-      const { HMSVBPlugin } = await import("@100mslive/hms-virtual-background");
-      pluginRef.current = new HMSVBPlugin(
-        HMSVirtualBackgroundTypes.NONE,
-        HMSVirtualBackgroundTypes.NONE
-      );
+      // const { HMSVBPlugin } = await import("@100mslive/hms-virtual-background");
+      // pluginRef.current = new HMSVBPlugin(
+      //   HMSVirtualBackgroundTypes.NONE,
+      //   HMSVirtualBackgroundTypes.NONE
+      // );
+      pluginRef.current = new TestPlugin();
     }
   }
   useEffect(() => {
@@ -47,9 +51,9 @@ export const VirtualBackground = () => {
   async function addPlugin() {
     try {
       await createPlugin();
-      window.HMS.virtualBackground = pluginRef.current;
-      const { background, backgroundType } = getRandomVirtualBackground();
-      await pluginRef.current.setBackground(background, backgroundType);
+      // window.HMS.virtualBackground = pluginRef.current;
+      // const { background, backgroundType } = getRandomVirtualBackground();
+      // await pluginRef.current.setBackground(background, backgroundType);
       await hmsActions.addPluginToVideoTrack(
         pluginRef.current,
         Math.floor(role.publishParams.video.frameRate / 2)
