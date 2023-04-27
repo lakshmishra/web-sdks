0294978a6968f195814d2efd2609f86cad9a46b9
raviteja83 <ravi.theja@100ms.live>
2023-04-27 14:32:06 +0530

    fix: revert to drawImage

--- apps/100ms-web/src/plugins/VirtualBackground/TestPlugin.js
+++ apps/100ms-web/src/plugins/VirtualBackground/TestPlugin.js
@@ -17,21 +17,21 @@ export class TestPlugin {
     // ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
     // ctx.fillRect(0, 0, input.width, input.height);
     // ctx.globalCompositeOperation = "destination-over";
-    const imgData = input
-      .getContext("2d")
-      ?.getImageData(0, 0, output.width, output.height);
-    ctx.putImageData(imgData, 0, 0);
-    // ctx.drawImage(
-    //   input,
-    //   0,
-    //   0,
-    //   input.width,
-    //   input.height,
-    //   0,
-    //   0,
-    //   input.width,
-    //   input.height
-    // );
+    // const imgData = input
+    //   .getContext("2d")
+    //   ?.getImageData(0, 0, output.width, output.height);
+    // ctx.putImageData(imgData, 0, 0);
+    ctx.drawImage(
+      input,
+      0,
+      0,
+      input.width,
+      input.height,
+      0,
+      0,
+      input.width,
+      input.height
+    );
   }
 
   getName() {
