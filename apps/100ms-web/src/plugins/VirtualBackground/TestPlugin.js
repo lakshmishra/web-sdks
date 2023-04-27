248853962fafb82637e2604349061a82a088d95c
raviteja83 <ravi.theja@100ms.live>
2023-04-27 13:50:24 +0530

    test: use image data

--- apps/100ms-web/src/plugins/VirtualBackground/TestPlugin.js
+++ apps/100ms-web/src/plugins/VirtualBackground/TestPlugin.js
@@ -12,17 +12,19 @@ export class TestPlugin {
     ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
     ctx.fillRect(0, 0, input.width, input.height);
     ctx.globalCompositeOperation = "destination-over";
-    ctx.drawImage(
-      input,
-      0,
-      0,
-      input.width,
-      input.height,
-      0,
-      0,
-      input.width,
-      input.height
-    );
+    const imgData = input.getContext("2d")?.getImageData();
+    ctx.putImageData(imgData, 0, 0);
+    // ctx.drawImage(
+    //   input,
+    //   0,
+    //   0,
+    //   input.width,
+    //   input.height,
+    //   0,
+    //   0,
+    //   input.width,
+    //   input.height
+    // );
   }
 
   getName() {
