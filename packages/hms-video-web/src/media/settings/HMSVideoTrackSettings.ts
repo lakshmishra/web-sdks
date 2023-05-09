4a3b38d9bda8bf9dbfcb7469d246f90a6503d36c
raviteja83 <ravi.theja@100ms.live>
2023-05-09 15:14:54 +0530

    fix: update width/height

--- packages/hms-video-web/src/media/settings/HMSVideoTrackSettings.ts
+++ packages/hms-video-web/src/media/settings/HMSVideoTrackSettings.ts
@@ -119,8 +119,8 @@ export class HMSVideoTrackSettings implements IHMSVideoTrackSettings, IAnalytics
     }
     const isPortrait = isIOS() && window?.innerWidth < window?.innerHeight;
     return {
-      width: { [dimensionConstraintKey]: this.width },
-      height: { [dimensionConstraintKey]: this.height },
+      width: { [dimensionConstraintKey]: isPortrait ? this.height : this.width },
+      height: { [dimensionConstraintKey]: isPortrait ? this.width : this.height },
       frameRate: this.maxFramerate,
       aspectRatio: isPortrait ? (this.height || 1) / (this.width || 1) : (this.width || 1) / (this.height || 1),
       deviceId: this.deviceId,
