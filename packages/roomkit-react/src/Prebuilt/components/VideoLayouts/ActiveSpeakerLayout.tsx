7586c15a730c0e1f7005af27dc9f7f7390bb2c91
raviteja83 <ravi.theja@100ms.live>
2024-02-10 14:04:12 +0530

    fix: css

--- packages/roomkit-react/src/Prebuilt/components/VideoLayouts/ActiveSpeakerLayout.tsx
+++ packages/roomkit-react/src/Prebuilt/components/VideoLayouts/ActiveSpeakerLayout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { usePrevious } from 'react-use';
 import { selectDominantSpeaker, selectPeers, useHMSStore } from '@100mslive/react-sdk';
 import { Box, Flex } from '../../../Layout';
 import VideoTile from '../VideoTile';
@@ -6,16 +7,17 @@ import VideoTile from '../VideoTile';
 export const ActiveSpeakerLayout = () => {
   const peers = useHMSStore(selectPeers);
   const dominantSpeaker = useHMSStore(selectDominantSpeaker);
+  const previousSpeaker = usePrevious(dominantSpeaker);
 
   return (
     <Flex css={{ size: '100%' }}>
-      <Flex direction="column" css={{ w: 240 }}>
+      <Flex direction="column" css={{ w: 240, '@md': { display: 'none' } }}>
         {peers.map(peer => {
           return <VideoTile peerId={peer.id} />;
         })}
       </Flex>
       <Box css={{ flex: '1 1 0', overflow: 'hidden' }}>
-        <VideoTile width="100%" height="100%" peerId={dominantSpeaker?.id} />
+        <VideoTile peerId={dominantSpeaker?.id || previousSpeaker?.id} rootCSS={{ maxHeight: '100%' }} />
       </Box>
     </Flex>
   );
